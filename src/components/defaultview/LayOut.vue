6d1bee094a456b3ece20e2b8d877a6e47ace03a2
othniel2471 <othnielomaidu@gmail.com>
2022-12-27 20:30:00 +0100

    fourth one

---
+++ src/components/defaultview/LayOut.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="main">
+    <!-- <h1>sir</h1> -->
+    <NavBar />
+    <SideBar />
+  </div>
+</template>
+
+<script setup>
+import NavBar from "./NavBar.vue";
+import SideBar from "./SideBar.vue";
+</script>
+
+<style scoped>
+.main {
+  width: 100%;
+}
+</style>
