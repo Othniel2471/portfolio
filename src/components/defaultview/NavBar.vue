cd4d89138bf6c86fd931775d1a4d336c894195a0
othniel2471 <othnielomaidu@gmail.com>
2023-01-05 23:33:37 +0100

    portfolioView

--- src/components/defaultview/NavBar.vue
+++ src/components/defaultview/NavBar.vue
@@ -1,11 +1,10 @@
-<template>
+<!-- <template>
   <div class="navigation">
     <nav>
       <div class="nav-header">
         <div class="menu-toggle"></div>
-        <!-- <h1>othniel omaidu</h1> -->
       </div>
-      <ul class="nav-links">
+      <ul class="nav-links links">
         <li class="link hover-underline-animation">
           <router-link class="hov" to="/">About</router-link>
         </li>
@@ -18,14 +17,10 @@
       </ul>
     </nav>
   </div>
-</template>
-
-<script>
-export default {};
-</script>
+</template> -->
 
 <style scoped>
-.navigation {
+/* .navigation {
   width: 100%;
 }
 nav {
@@ -36,7 +31,6 @@ nav {
   border: 1px solid #6d8093;
 }
 nav a {
-  /* font-weight: bold; */
   color: #5671a6;
   text-decoration: none;
   list-style: none;
@@ -88,10 +82,8 @@ nav a {
   color: #b3cbd9;
 }
 
-/* media query */
 @media only screen and (min-width: 565px) {
   nav .nav-header h1 {
-    position: absolute;
     width: 25%;
     height: 54px;
     left: 20px;
@@ -109,7 +101,6 @@ nav a {
     flex-direction: row;
     align-items: flex-start;
     padding: 0px;
-    /* gap: 47px; */
     text-transform: uppercase;
     position: absolute;
     width: 250px;
@@ -126,5 +117,200 @@ nav a {
   .menu-toggle {
     display: none;
   }
-} ;
+} ; */
+</style>
+
+<template>
+  <div>
+    <nav>
+      <div class="nav-center">
+        <!-- nav header -->
+        <div class="nav-header">
+          <!-- <img src="./logo.svg" class="logo" alt="logo" /> -->
+          <button class="nav-toggle" @click="toggleNav">
+            <i class="fas fa-bars"></i>
+          </button>
+        </div>
+        <!-- links -->
+        <ul class="links" :class="{ show_links: isActive }">
+          <ul class="nav-links links">
+            <li
+              class="hover-underline-animation"
+              :class="{ linkz: showLinks, link1: linkPos1 }"
+            >
+              <router-link class="hov" to="/">About</router-link>
+            </li>
+            <li
+              class="hover-underline-animation"
+              :class="{ linkz: showLinks, link2: linkPos2 }"
+            >
+              <router-link class="hov" to="/skills">skills</router-link>
+            </li>
+            <li
+              class="hover-underline-animation"
+              :class="{ linkz: showLinks, link3: linkPos3 }"
+            >
+              <router-link class="hov" to="/portfolio">portfolio</router-link>
+            </li>
+          </ul>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+
+const isActive = ref(null);
+const showLinks = ref(null);
+const linkPos1 = ref(null);
+const linkPos2 = ref(null);
+const linkPos3 = ref(null);
+
+onMounted(() => {
+  window.addEventListener("resize", checkScreen);
+  checkScreen();
+});
+
+const toggleNav = () => {
+  isActive.value = !isActive.value;
+  showLinks.value = !showLinks.value;
+  linkPos1.value = !linkPos1.value;
+  linkPos2.value = !linkPos2.value;
+  linkPos3.value = !linkPos3.value;
+};
+
+const checkScreen = () => {
+  if (window.innerWidth > 600) {
+    isActive.value = false;
+    showLinks.value = false;
+    linkPos1.value = false;
+    linkPos2.value = false;
+    linkPos3.value = false;
+  }
+};
+</script>
+
+<style scoped>
+nav {
+  background: var(--clr-white);
+  box-shadow: var(--light-shadow);
+  border: 1px solid #6d8093;
+}
+.nav-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1rem;
+}
+.nav-toggle {
+  font-size: 1.5rem;
+  color: var(--clr-primary-5);
+  background: transparent;
+  border-color: transparent;
+  transition: var(--transition);
+  cursor: pointer;
+}
+.nav-toggle:hover {
+  color: var(--clr-primary-1);
+  transform: rotate(90deg);
+}
+
+.links a {
+  position: relative;
+  color: var(--clr-grey-3);
+  font-size: 1rem;
+  text-transform: capitalize;
+  letter-spacing: var(--spacing);
+  display: block;
+  padding: 0.5rem 1rem;
+  transition: var(--transition);
+  z-index: 200000;
+}
+.links a:hover {
+  background: var(--clr-primary-8);
+  color: var(--clr-primary-5);
+  padding-left: 1.5rem;
+}
+.social-icons {
+  display: none;
+}
+.links {
+  height: 0;
+  overflow: hidden;
+  transition: var(--transition);
+}
+.show_links {
+  height: 7rem;
+  /* transform: translateX(-64px); */
+  transform: translate(-64px, -38px);
+}
+.linkz {
+  position: absolute;
+}
+.link1 {
+  transform: translateY(1rem);
+}
+.link2 {
+  transform: translateY(3rem);
+}
+.link3 {
+  transform: translateY(5rem);
+}
+@media screen and (min-width: 800px) {
+  .nav-center {
+    max-width: 1170px;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 1rem;
+  }
+  .nav-header {
+    padding: 0;
+  }
+  .nav-toggle {
+    display: none;
+  }
+  .links {
+    height: auto;
+    display: flex;
+  }
+  .links a {
+    padding: 0;
+    margin: 0 0.5rem;
+  }
+  .links a:hover {
+    padding: 0;
+    background: transparent;
+  }
+  .hover-underline-animation {
+    display: inline-block;
+    position: relative;
+    color: #b3cbd9;
+  }
+
+  .hover-underline-animation:after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    transform: scaleX(0);
+    height: 2px;
+    bottom: 0;
+    left: 0;
+    background-color: #b3cbd9;
+    transform-origin: bottom right;
+    transition: transform 0.25s ease-out;
+  }
+
+  .hover-underline-animation:hover:after {
+    transform: scaleX(1);
+    transform-origin: bottom left;
+    background-color: #b3cbd9;
+  }
+  .hov:hover {
+    color: #b3cbd9;
+  }
+}
 </style>
--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,6 +1,92 @@
 <template>
-  <div>
-    <h1>portfilio</h1>
+  <div class="portfolio">
+    <div class="portfolio-body">
+      <h1 class="header">portfolio</h1>
+      <div class="wrapper">
+        <div class="box">
+          <h3>STICKY</h3>
+          <div class="img">
+            <img src="../../public/images/projects/sticky.png" alt="" />
+          </div>
+          <div class="content">
+            <p>
+              Sticky is a simple note app, that i developed with "vue". The
+              users gets the chance to add new notes. The notes are generated
+              and given random colors, the user also has the ability to delete
+              notes and also change the theme of each note with the color
+              choices provided
+            </p>
+            <br />
+            <a
+              href="https://othniel2471.github.io/stickyNotes/"
+              class="btns"
+              target="_blank"
+              >Live Demo</a
+            >
+          </div>
+        </div>
+
+        <div class="box">
+          <h3>MATCHAPP</h3>
+          <div class="img">
+            <img src="../../public/images/projects/matchapp.png" alt="" />
+          </div>
+          <div class="content">
+            <p>
+              The MatchApp is an app i developed when i wanted to make it
+              possible to share christmas presents with my family, and we needed
+              to get names randomly, so i built this simple app that takes in my
+              family members names and also randomly picks two people and match
+              them together.
+            </p>
+            <br />
+            <a
+              href="https://othniel2471.github.io/matchApp/"
+              target="_blank"
+              class="btns"
+              >Live Demo</a
+            >
+          </div>
+        </div>
+        <div class="box">
+          <h3>Intro Section</h3>
+          <div class="img">
+            <img src="../../public/images/projects/introsection.png" alt="" />
+          </div>
+          <div class="content">
+            <p>
+              This is the hero section of a home page for an app. I built it
+              with vanilla js and tailwind css, it is fully responsive. I got
+              the excercise from frontend mentor
+            </p>
+            <br />
+            <a
+              href="https://tailwindherosection.netlify.app/"
+              class="btns"
+              target="_blank"
+              >Live Demo</a
+            >
+          </div>
+        </div>
+        <div class="box">
+          <h3>OT - SAMA</h3>
+          <div class="img">
+            <img src="../../public/images/css1.png" alt="" />
+          </div>
+          <div class="content">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Recusandae deleniti sint consequuntur quo amet! Esse
+              exercitationem repellendus atque eos cupiditate doloremque, natus
+              libero vitae quod officia consequatur assumenda cumque odit omnis
+              fuga? Laboriosam accusantium eaque sit saepe alias aliquam!
+            </p>
+            <br />
+            <a href="#" class="btns">Live Demo</a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -8,4 +94,69 @@
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.portfolio {
+  width: 100%;
+  margin-top: 1rem;
+}
+.portfolio-body {
+  display: grid;
+  place-items: center;
+  text-transform: capitalize;
+}
+.header {
+  font-weight: 600;
+  line-height: 36px;
+  background: linear-gradient(101.83deg, #5671a6 14.93%, #b2cad8 58.66%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  text-transform: capitalize;
+}
+.wrapper {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 300px);
+  padding: 24px;
+  gap: 20px;
+  justify-content: center;
+  width: 100%;
+}
+.box {
+  background: #fff;
+  padding: 20px;
+  border-radius: 2rem;
+}
+.box:hover {
+  transform: translateY(-10px);
+  transition: 0.5s;
+  cursor: pointer;
+}
+.box .img {
+  border-radius: 1rem;
+  width: 100%;
+}
+
+.box h3 {
+  font-size: 1rem;
+  font-weight: 600;
+  float: right;
+}
+.content {
+  /* width: 20rem; */
+  font-size: 13px;
+  padding: 10px;
+  margin-bottom: 0.5rem;
+  text-align: justify;
+  box-shadow: 0 10px 20px 3px #00968814;
+  /* filter: blur(3px); */
+}
+.btns {
+  background: #000;
+  color: #fff;
+  border-radius: 0.5rem;
+  padding: 5px;
+}
+.btns:hover {
+  background: #333;
+}
+</style>
