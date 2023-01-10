78aff3fa23364b293f8645419327daad4f8bab67
othniel2471 <othnielomaidu@gmail.com>
2023-01-10 14:08:05 +0100

    second commit

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,5 @@
-
+const { defineConfig } = require("@vue/cli-service");
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/portfolio-site/" : "/",
+});
